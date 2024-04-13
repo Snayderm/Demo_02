11c692b7adb4fe57c79f87071fce80bc0cc1fdb5
Snayderm <sneyder1002m@gmail.com>
2024-04-13 03:14:30 -0500

    Constructor de Clase Base. (Estilo Antiguo)

--- MyLibrary/Book.cs
+++ MyLibrary/Book.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,20 +7,13 @@ using System.Threading.Tasks;
 
 namespace MyLibrary
 {
-    public class Book : Product
-    {
-        public string Title { get; set; }
-        public Book(int id, string name, decimal unitPrice, int unitsInStock, string title) :
-            base(id, name, unitPrice, unitsInStock)
-        {
-            Title = title;
-        }
-    }
-    
-    public class Book2(int id, string name, decimal unitPrice, int unitsInStock, string title) :
-        Product(id, name, unitPrice, unitsInStock)
-    {
-        public string Title => Title;
-
-    }
+	public class Book : Product
+	{
+		public string Title { get; set; }
+		public Book(int id, string name, decimal unitPrice, int unitsInStock, string title) :
+			base(id, name, unitPrice, unitsInStock)
+		{
+			Title = title;
+		}
+	}
 }
