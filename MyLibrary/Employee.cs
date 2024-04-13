cd5f3de5aacb33c98d7cd766835672f64d85c935
Snayderm <sneyder1002m@gmail.com>
2024-04-13 03:53:00 -0500

    Metodo Abstract en la Clase Manager

--- MyLibrary/Employee.cs
+++ MyLibrary/Employee.cs
@@ -10,17 +10,24 @@ namespace MyLibrary
 	{
 		public int Id { get; set; }
 		public string Name { get; set; }
-		
+
+		public int GetInt()
+		{
+			return 5;
+		}q
 		public abstract decimal CalculateMonthlyPayment();
 	}
 
 	public class Secretary : Employee
 	{
-
+		public override decimal CalculateMonthlyPayment() => 15000;
 	}
 
 	public class Manager : Employee
 	{
-
-	}
+		public override decimal CalculateMonthlyPayment()
+		{
+			return 30000;
+		}
+	}						 
 }
