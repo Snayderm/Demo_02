e36deb93fab75936acee489f9cffc7604c309f7e
Snayderm <sneyder1002m@gmail.com>
2024-04-13 03:31:30 -0500

    Clase Employee

--- MyLibrary/Employee.cs
+++ MyLibrary/Employee.cs
@@ -6,45 +6,23 @@ using System.Threading.Tasks;
 
 namespace MyLibrary
 {
-    public abstract class Employee
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        
-        public int GetInt() 
-        {
-            return 5;
-        }
-        public virtual decimal CalculateMonthlyPayment()
-        {
-            return 12000;
-        }
-        
-        public int GetPaymentDays()
-        {
-            return 30;
-        }
-    }
+	public class Employee
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public decimal CalculateMonthlyPayment()
+		{
 
-    public class Secretary : Employee 
-    {
-        public override decimal CalculateMonthlyPayment() => 15000;
+		}
+	}
 
-    }
+	public class Secretary : Employee
+	{
 
-    public class Manager : Employee
-    {
-        public override decimal CalculateMonthlyPayment()
-        {
-            return 30000;
-        }
-    }
-    
-    public class Developer : Employee
-    {
-        public new int GetPaymentDays() 
-        {
-            return 20;
-        }
-    }
+	}
+
+	public class Manager : Employee
+	{
+
+	}
 }
